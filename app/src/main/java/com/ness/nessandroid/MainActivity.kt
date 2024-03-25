6c5d0feeb35aefb5874776e1829c3a3809d1a89a
KindnessKami <kubaikindness@gmail.com>
2024-03-25 12:17:11 +0300

    initial commit

---
+++ app/src/main/java/com/ness/nessandroid/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.ness.nessandroid
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ness.nessandroid.ui.theme.NessandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NessandroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                 //   Greeting("Android")
+                   Login()
+                }
+            }
+        }
+    }
+}
+
+//
+
+@Composable
+fun Login() {
+    var email by remember {(mutableStateOf(""))}
+    var password by remember {(mutableStateOf(""))}
+    var context= LocalContext.current
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .background(Color.Black)) {
+        Text(text = "Meraki",
+            color = Color.Gray,
+            fontSize = 30.sp,
+            fontFamily = FontFamily.Cursive
+        )
+OutlinedTextField(value =password,
+    onValueChange ={password = it},
+    leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription =password )},
+    label = { Text(text = "Password")},
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 8.dp),
+    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Default),
+    keyboardActions = KeyboardActions()
+)
+
+OutlinedTextField(value = email,
+    onValueChange ={email = it},
+    leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = email)},
+    label = { Text(text = "Email Address")},
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 8.dp),
+    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Default),
+    keyboardActions = KeyboardActions()
+        )
+Button(onClick = { /*TODO*/ },
+    modifier = Modifier.fillMaxWidth()) {
+
+    Text(text = "Log in",
+        color = Color.Gray
+    )
+}
+Button(onClick = {
+    val Intent=Intent(context,MainActivity::class.java)
+    context.startActivity(Intent)
+}) {
+    Text(text = "Sign in")
+
+}
+    }
+    
+}
+@Preview
+@Composable
+private fun Loginprev() {
+    Login()
+
+}
